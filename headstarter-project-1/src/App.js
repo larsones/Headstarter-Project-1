7a3a0c4356da2a123733b4542310a73dc6aaa62c
Larson, Eric S <larsones@iu.edu>
2023-07-09 14:23:32 -0400

    Update App.js adding buttons
    Currently made 2 basic buttons on the program

--- headstarter-project-1/src/App.js
+++ headstarter-project-1/src/App.js
@@ -19,12 +19,22 @@ function App() {
       fontSize: '2rem',
       marginBottom: '2rem',
     },
+    button1: {
+      backgroundColor: 'white',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      color: 'navy',
+    }
   };
 
   return (
     <div style={styles.container}>
       <h1 style={styles.header}>Resume Parser</h1>
       {/* Other components and content */}
+      <button style={styles.button1}>Upload Resume</button> 
+      <button style={styles.button1}>Filter Resume</button> 
+
     </div>
   );
 }
