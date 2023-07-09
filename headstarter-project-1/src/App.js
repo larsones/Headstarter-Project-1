d1b67d3956aa6690194d78dee762f5a4796ee7c4
Larson, Eric S <larsones@iu.edu>
2023-07-09 15:14:48 -0400

    Adding files enabled starting on list of all

--- headstarter-project-1/src/App.js
+++ headstarter-project-1/src/App.js
@@ -25,15 +25,26 @@ function App() {
       justifyContent: 'center',
       alignItems: 'center',
       color: 'navy',
-    }
+    },
   };
+
+  function testn(){
+    alert("Click Counted")
+  }
 
   return (
     <div style={styles.container}>
       <h1 style={styles.header}>Resume Parser</h1>
       {/* Other components and content */}
-      <button style={styles.button1}>Upload Resume</button> 
-      <button style={styles.button1}>Filter Resume</button> 
+      {/*<v */}
+      <input  type='file' name='file' onChange={testn}/>
+      <div>
+        <button style={styles.button1} onClick={testn} >Upload Resume</button> 
+        
+        <button style={styles.button1}>Filter Resume</button> 
+
+      </div> 
+      {/*Possibly a List that is based off an array of added resumes so would be empty at start and grow with inputs to show file upload was a success */}
 
     </div>
   );
